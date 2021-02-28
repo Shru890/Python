53eb6c2824698fca9ff0e0624c94cd81c378d024
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2021-02-28 17:08:02 +0530

    Create Equivalents.py

---
+++ Equivalents.py
@@ -0,0 +1,35 @@
+#equivalents of data types in numpy
+import numpy as np 
+#boolean
+dt = np.dtype('bool_')
+print dt
+#integers
+dt = np.dtype('i1')
+print dt
+dt = np.dtype('i2')
+print dt
+dt = np.dtype('i4')
+print dt
+dt = np.dtype('i8')
+print dt
+#unsigned integers
+dt = np.dtype('u1')
+print dt
+dt = np.dtype('u2')
+print dt
+dt = np.dtype('u4')
+print dt
+dt = np.dtype('u8')
+print dt
+#float
+dt = np.dtype('f2')
+print dt
+dt = np.dtype('f4')
+print dt
+dt = np.dtype('f8')
+print dt
+#complex numbers
+dt = np.dtype('c8')
+print dt
+dt = np.dtype('c16')
+print dt
