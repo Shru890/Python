28d2709757ce46668b0b71cd187b22da8203f741
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2021-03-05 22:27:49 +0530

    Update random.py

--- random.py
+++ random.py
@@ -15,3 +15,21 @@ if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
 
+#minion game hackerrank
+ vowel =['A','E','I','O','U']
+    S=0
+    K=0
+    for i in range(len(string)):
+        if string[i] in vowel:
+            K+= len(string)-i
+        else:
+            S+=len(string)-i
+    if S>K:
+        print("Stuart"+" "+ "%d" % S)
+    elif K>S:
+        print("Kevin"+" "+'%d' % K)
+    else:
+        print("Draw")
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
