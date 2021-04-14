3df8da425d418eb3c90b2476dcae51cfed1ecd6b
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2021-04-14 19:35:24 +0530

    Update geeksforgeeks.py

--- geeksforgeeks.py
+++ geeksforgeeks.py
@@ -19,3 +19,26 @@ if __name__ == "__main__":
 		ans=ob.get(a,b)
 		print(str(ans[0])+" "+str(ans[1]))
 #} Driver Code Ends
+
+#Number of 1 Bits
+class Solution:
+	def setBits(self, N):
+		# code here
+		count=0
+		N=bin(N).replace("0b", "")
+		for i in N:
+		    if i=="1":
+		        count+=1
+		return count
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		ob = Solution()
+		ans = ob.setBits(N)
+		print(ans)
