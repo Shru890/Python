781fb858499d0912bdb302f16a78fbfbb8c12256
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2021-04-23 12:07:40 +0530

    Create pangram.py

---
+++ pangram.py
@@ -0,0 +1,16 @@
+def is_pangram(sentence):
+	sentence=str(sentence)
+	alpha=[chr(i) for i in range(97,123)]
+	num=[chr(i)for i in range(48,58)]
+	sentence=sentence.lower()
+	sentence = [i for i in sentence if i in alpha]
+	sentence=(set(sentence))
+	sentence=sorted(sentence)
+	sentence = [i for i in sentence if i not in num]
+	if sentence == alpha:
+		return True
+	else:
+		return False
+sentence=input().replace(".","")
+ans=pangram(sentence)
+print(ans)
